a24726da0d206c85079a26cc5252460b50db5107
1820982382 <ykx1820982382@outlook.com>
2026-06-22 21:45:05 +0800

    新建1.21.1-neoforge版本，并新增按键控制功能。

--- _fix_gui.py
+++
@@ -1,8 +0,0 @@
-﻿import os
-base = 'F:/Storage/ai/create-qbp-1.21.1-neoforge'
-path = os.path.join(base, 'src/main/java/org/kdvcs/vbm/client/GuiOverlay.java')
-with open(path, 'r', encoding='utf-8') as f:
-    content = f.read()
-print('Length:', len(content))
-print('First:', repr(content[:100]))
-
